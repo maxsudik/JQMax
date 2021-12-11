fd993757840d8c4c698df8a9e92eded4547aac11
maxsudik <maxsudik@gmail.com>
2021-12-11 01:13:54 -0800

    Adding local properties file

---
+++ src/main/java/BaseTest.java
@@ -0,0 +1,10 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class BaseTest {
+
+    public void initializeDriver() {
+        Properties properties = new Properties();
+        FileInputStream file = new FileInputStream("");
+    }
+}
