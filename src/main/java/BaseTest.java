ae3cf9920de01d65bc76361d5a5f9d8fa54b8c60
maxsudik <maxsudik@gmail.com>
2021-12-11 18:08:25 -0800

    Adding webdrivermanager to manage drivers for selenium

--- src/main/java/BaseTest.java
+++ src/main/java/BaseTest.java
@@ -1,10 +1,30 @@
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 public class BaseTest {
 
-    public void initializeDriver() {
+    public void initializeDriver() throws IOException {
+
+        //TODO add webdriver manager
         Properties properties = new Properties();
-        FileInputStream file = new FileInputStream("");
+        FileInputStream file = new FileInputStream("src/main/resources/local.properties");
+        properties.load(file);
+        String browser = properties.getProperty("browser");
+
+        if (browser.equals("chrome")) {
+
+            //TODO define condition
+        } else if (browser.equals("edge")) {
+            //TODO define condition
+
+        } else if (browser.equals("safari")) {
+            //TODO define condition
+
+        } else if (browser.equals("firefox")) {
+            //TODO define condition
+
+        }
+
     }
 }
