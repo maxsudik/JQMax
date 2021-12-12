66508bbbe6fd0ca48c9bccf3bd01e2bf722532c8
maxsudik <maxsudik@gmail.com>
2021-12-11 21:19:47 -0800

    Creating object of POM class in order to get methods and objects from that class

--- src/test/java/HomePageTest.java
+++ src/test/java/HomePageTest.java
@@ -1,11 +1,15 @@
+import base.BaseTest;
+import model.LandingPage;
 import org.testng.annotations.Test;
 import java.io.IOException;
 
-public class HomePage extends BaseTest {
+public class HomePageTest extends BaseTest {
 
     @Test
     public void basePageNavigation() throws IOException, InterruptedException {
         driver = initializeDriver();
         driver.get("http://the-internet.herokuapp.com/");
+        LandingPage landingPage = new LandingPage(driver);
+        landingPage.getAbTestingItem().click();
     }
 }
