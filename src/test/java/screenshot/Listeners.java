8d5ca16270df828cdeeebf78de3cb9545a74f70d
maxsudik <maxsudik@gmail.com>
2021-12-13 12:33:53 -0800

    Adding Extent report capabilities in Listener class in order to populate report on test start/failure/success/finish Adding Thread safe capability for parallel execution Adding screenshots to the report

--- src/test/java/screenshot/Listeners.java
+++ src/test/java/screenshot/Listeners.java
@@ -1,7 +1,12 @@
 package screenshot;
 
 import base.BaseTest;
+import base.ExtentReporterNG;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -9,10 +14,26 @@ import java.io.IOException;
 
 public class Listeners extends BaseTest implements ITestListener {
 
+    ExtentTest test;
+    ExtentReports extent = ExtentReporterNG.getReportObject();
+    ThreadLocal<ExtentTest> extentTest = new ThreadLocal();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        test = extent.createTest(result.getMethod().getMethodName());
+        extentTest.set(test);
+    }
+
     //TODO implement screenshot on test completion
     @Override
     public void onTestSuccess(ITestResult result) {
+        extentTest.get().log(Status.PASS, "Test Passed");
 
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
     }
 
     @Override
@@ -21,13 +42,15 @@ public class Listeners extends BaseTest implements ITestListener {
         WebDriver driver = null;
         String testMethodName = result.getMethod().getMethodName();
 
+        extentTest.get().fail(result.getThrowable());
+
         try {
-            driver = (WebDriver)result.getTestClass().getRealClass().getDeclaredField("driver").get(result.getInstance());
+            driver = (WebDriver) result.getTestClass().getRealClass().getDeclaredField("driver").get(result.getInstance());
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
-            getScreenshot(testMethodName, driver);
+            extentTest.get().addScreenCaptureFromPath(getScreenshot(testMethodName, driver), result.getMethod().getMethodName());
         } catch (IOException e) {
             e.printStackTrace();
         }
