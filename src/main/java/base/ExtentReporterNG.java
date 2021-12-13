22706d7b278d89c95e34a9a532f5aa4b03f7410b
maxsudik <maxsudik@gmail.com>
2021-12-13 12:30:48 -0800

    Adding configuration class for Extent Report

---
+++ src/main/java/base/ExtentReporterNG.java
@@ -0,0 +1,20 @@
+package base;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporterNG {
+    static ExtentReports extent;
+
+    public static ExtentReports getReportObject() {
+        String path = System.getProperty("user.dir") + "/reports/index.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setReportName("Results");
+        reporter.config().setDocumentTitle("Test Results");
+
+        extent = new ExtentReports();
+        extent.attachReporter(reporter);
+        extent.setSystemInfo("Tester", "Max Sudik");
+        return extent;
+    }
+}
