86acfc9221be91e257ea2b2d055c1c0e58501c8d
maxsudik <40281293+maxsudik@users.noreply.github.com>
2021-12-20 20:05:59 -0800

    34 add Github action on pull main workflow (#35)
    * Adding GitHub Action to run workflow on PR to main
    
    * Adding Chrome Options to run on linux Server

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
@@ -27,9 +28,15 @@ public class BaseTest {
         properties.load(file);
         String browser = properties.getProperty("browser");
 
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-gpu");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+
         if (browser.equals("chrome")) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
         } else if (browser.equals("edge")) {
             WebDriverManager.edgedriver().setup();
             driver = new EdgeDriver();
