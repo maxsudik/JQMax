645d41f0c114fd5781f3dd7583eb1ef5d2748a3f
maxsudik <40281293+maxsudik@users.noreply.github.com>
2022-01-02 01:23:40 -0800

    42 check thread safetenest (#43)
    * Adding A/B Test page model
    
    * Changing locator for the title
    
    * Adding dummy tests in order to check thread safeness
    
    * Cleaning up unnecessary exception
    
    * Adding TODO in order to implement headless mode only on server run
    
    * Adding second test to testNG in order to run test in parallel
    
    * Adding driver manager for managing driver instance as Thread Local
    
    * Refactoring Driver class in order to instantiate driver using DriverManager
    
    * Removing POM from tests to verify thread safeness of tests
    
    * Adding parallel thread count 2 to check thread safeness

---
+++ src/test/java/model/ABTestingPage.java
@@ -0,0 +1,25 @@
+package model;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ABTestingPage {
+
+    public WebDriver driver;
+
+    private By pageTitle = By.xpath("//h3");
+    private By pageText = By.xpath("//p");
+
+    public ABTestingPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public WebElement getABTestPageTitle() {
+        return driver.findElement(pageTitle);
+    }
+
+    public WebElement getABTestPageText() {
+        return driver.findElement(pageText);
+    }
+}
---
+++ src/test/java/tests/ABTestingTest.java
@@ -0,0 +1,26 @@
+package tests;
+
+import driver.DriverManager;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public final class ABTestingTest extends BaseTest {
+
+    private ABTestingTest() {
+    }
+
+    @Test
+    public void verifyABTestPageTitle() throws InterruptedException {
+        DriverManager.getDriver().findElement(By.xpath("//a[@href='/abtest']")).click();
+        String abTestPageTitle = DriverManager.getDriver().findElement(By.xpath("//h3")).getText();
+        Assert.assertEquals(abTestPageTitle, "A/B Test Variation 1");
+    }
+
+    @Test
+    public void verifyABTestPageText() {
+        DriverManager.getDriver().findElement(By.xpath("//a[@href='/abtest']")).click();
+        String abTestPageText = DriverManager.getDriver().findElement(By.xpath("//p")).getText();
+        Assert.assertEquals(abTestPageText, "Also known as split testing. This is a way in which businesses are able to simultaneously test and learn different versions of a page to see which text and/or functionality works best towards a desired outcome (e.g. a user action such as a click-through).");
+    }
+}
