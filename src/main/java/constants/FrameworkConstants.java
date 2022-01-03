1e88c21c98cb60835f6c351866021cb5c7b512aa
maxsudik <maxsudik@gmail.com>
2022-01-03 13:14:11 -0800

    Adding constants class in order to manage global constants of the framework

---
+++ src/main/java/constants/FrameworkConstants.java
@@ -0,0 +1,13 @@
+package constants;
+
+public final class FrameworkConstants {
+    private static final int EXPLICIT_WAIT = 10;
+
+    private FrameworkConstants() {
+    }
+
+    public static int getExplicitWait() {
+        return EXPLICIT_WAIT;
+    }
+
+}
