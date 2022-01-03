cf9a00eda1b9acb15312107c058bf4408f55e4c0
maxsudik <maxsudik@gmail.com>
2022-01-03 14:17:47 -0800

    Moving logic related to the waiting strategy into ExplicitWaitFactory class

---
+++ src/main/java/factories/ExplicitWaitFactory.java
@@ -0,0 +1,34 @@
+package factories;
+
+import constants.FrameworkConstants;
+import driver.DriverManager;
+import enums.WaitStrategy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public final class ExplicitWaitFactory {
+
+    private ExplicitWaitFactory() {
+    }
+
+    public static WebElement performExplicitWait(By by, WaitStrategy waitStrategy) {
+        WebElement element = null;
+        if (waitStrategy == WaitStrategy.CLICKABLE) {
+            element = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(FrameworkConstants.getExplicitWait()))
+                    .until(ExpectedConditions.elementToBeClickable(by));
+        } else if (waitStrategy == WaitStrategy.PRESENCE) {
+            element = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(FrameworkConstants.getExplicitWait()))
+                    .until(ExpectedConditions.presenceOfElementLocated(by));
+        } else if (waitStrategy == WaitStrategy.VISIBLE) {
+            element = new WebDriverWait(DriverManager.getDriver(), Duration.ofSeconds(FrameworkConstants.getExplicitWait()))
+                    .until(ExpectedConditions.visibilityOfElementLocated(by));
+        } else if (waitStrategy == WaitStrategy.NONE) {
+            element = DriverManager.getDriver().findElement(by);
+        }
+        return element;
+    }
+}
