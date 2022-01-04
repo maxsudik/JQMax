9b99a21c6ea392aee6e9872f7393a757fe354b1e
maxsudik <maxsudik@gmail.com>
2022-01-03 22:03:36 -0800

    Removing login related to the reports creating. Already moved to ListenersClass

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,11 +5,9 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import reports.ExtentReport;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,15 +17,6 @@ public class BaseTest {
     protected BaseTest() {
     }
 
-    @BeforeSuite
-    public void beforeSuite() {
-        ExtentReport.initReport();
-    }
-
-    @AfterSuite
-    public void afterSuite() throws IOException {
-        ExtentReport.flushReports();
-    }
 
     @BeforeMethod
     protected void setUp() throws Exception {
