8dd893157d38e5f91f233539c89fda4137e077d7
maxsudik <40281293+maxsudik@users.noreply.github.com>
2022-01-03 18:22:22 -0800

    23 create tests with centralized data (#54)
    * Adding jackson dependencies in order to work with json files
    
    * Adding simple json file with AUT url
    
    * Adding utility to fetch url from config.json
    
    * Adding constants for storing pathes to properties and json configs
    
    * Changing the way to fetch url for AUT to fetch it from config.json file
    
    * Refactoring config.properties management
    
    * Adding dataprovider to the dummy test and set parallel thread count execution for the dataprovider to 2 threads

---
+++ src/main/java/utils/JsonUtils.java
@@ -0,0 +1,41 @@
+package utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import constants.FrameworkConstants;
+import enums.ConfigProperties;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+public class JsonUtils {
+
+    private static Map<String, String> CONFIG_MAP;
+
+    static {
+        try {
+            CONFIG_MAP = new ObjectMapper().readValue(new File(FrameworkConstants.getJsonConfigFilePath()),
+                    new TypeReference<HashMap<String, String>>() {
+                    });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private JsonUtils() {
+    }
+
+    public static String get(ConfigProperties key) throws Exception {
+        if (Objects.isNull(key) || Objects.isNull(CONFIG_MAP.get(key.name().toLowerCase(Locale.ROOT)))) {
+            throw new Exception("Property name " + key + " is not found. Please check config.properties");
+        }
+        return CONFIG_MAP.get(key.name().toLowerCase(Locale.ROOT));
+    }
+}
