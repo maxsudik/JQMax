85f2030d501f8bca0303946f4ddeaf52b9750880
maxsudik <maxsudik@gmail.com>
2022-01-03 18:18:46 -0800

    Adding constants for storing pathes to properties and json configs

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -1,7 +1,12 @@
 package constants;
 
 public final class FrameworkConstants {
+
+    private static final String RESOURCE_FILE_PATH = System.getProperty("user.dir") + "/src/test/resources";
+    private static final String CONFIG_FILE_PATH = RESOURCE_FILE_PATH + "/config/config.properties";
+    private static final String JSON_CONFIG_FILE_PATH = RESOURCE_FILE_PATH + "/config/config.json";
     private static final int EXPLICIT_WAIT = 10;
+
 
     private FrameworkConstants() {
     }
@@ -10,4 +15,11 @@ public final class FrameworkConstants {
         return EXPLICIT_WAIT;
     }
 
+    public static String getJsonConfigFilePath() {
+        return JSON_CONFIG_FILE_PATH;
+    }
+
+    public static String getConfigFilePath() {
+        return CONFIG_FILE_PATH;
+    }
 }
