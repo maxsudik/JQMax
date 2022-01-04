cb2af9d549e3a1e745e0c939cb41a0b66c8042ad
maxsudik <maxsudik@gmail.com>
2022-01-03 18:20:37 -0800

    Refactoring config.properties management

--- src/main/java/utils/PropertyUtils.java
+++ src/main/java/utils/PropertyUtils.java
@@ -1,5 +1,6 @@
 package utils;
 
+import constants.FrameworkConstants;
 import enums.ConfigProperties;
 
 import java.io.FileInputStream;
@@ -13,7 +14,7 @@ public final class PropertyUtils {
 
     static {
         try {
-            FileInputStream fileInputStream = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/config/local.properties");
+            FileInputStream fileInputStream = new FileInputStream(FrameworkConstants.getConfigFilePath());
             property.load(fileInputStream);
 
             for (Map.Entry<Object, Object> entry : property.entrySet()) {
@@ -27,32 +28,10 @@ public final class PropertyUtils {
     private PropertyUtils() {
     }
 
-    /**
-     * Use this method for retrieving large numbers of values from .property file
-     *
-     * @param key from .property file
-     * @return CONFIG_MAP of keys and values from .property file
-     * @throws Exception
-     */
-
     public static String get(ConfigProperties key) throws Exception {
-        if (Objects.isNull(key) || Objects.isNull(CONFIG_MAP.get(key))) {
-            throw new Exception("Property name " + key + " is not found. Please check local.properties");
+        if (Objects.isNull(key) || Objects.isNull(CONFIG_MAP.get(key.name().toLowerCase(Locale.ROOT)))) {
+            throw new Exception("Property name " + key + " is not found. Please check config.properties");
         }
         return CONFIG_MAP.get(key.name().toLowerCase(Locale.ROOT));
-    }
-
-    /**
-     * Use this method for retrieving small numbers of values from .property file
-     *
-     * @param key from .property file
-     * @return CONFIG_MAP of keys and values from .property file
-     * @throws Exception
-     */
-    public static String getValue(String key) throws Exception {
-        if (Objects.isNull(property.getProperty(key)) || Objects.isNull(key)) {
-            throw new Exception("Property name " + key + " is not found. Please check local.properties");
-        }
-        return property.getProperty(key);
     }
 }
