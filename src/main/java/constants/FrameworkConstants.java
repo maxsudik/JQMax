6ba07ee69dfaaa6e93d219f888e524ece8c5b56f
maxsudik <maxsudik@gmail.com>
2022-01-04 18:42:23 -0800

    Some minor changes for faster and clear code execution

--- src/main/java/constants/FrameworkConstants.java
+++ src/main/java/constants/FrameworkConstants.java
@@ -5,7 +5,7 @@ import utils.PropertyUtils;
 
 public final class FrameworkConstants {
 
-    private static final int EXPLICIT_WAIT = 10;
+    private static final int EXPLICIT_WAIT = 3;
     private static final String RESOURCE_FILE_PATH = System.getProperty("user.dir") + "/src/test/resources";
     private static final String CONFIG_FILE_PATH = RESOURCE_FILE_PATH + "/config/config.properties";
     private static final String JSON_CONFIG_FILE_PATH = RESOURCE_FILE_PATH + "/config/config.json";
