34a58fb492de9acfd6c13e2c9dd5d611432ff86d
maxsudik <maxsudik@gmail.com>
2022-01-05 19:53:33 -0800

    Creating custom annotation to annotate tests with parameters as "author" and "category"

---
+++ src/main/java/annotations/FrameworkAnnotation.java
@@ -0,0 +1,15 @@
+package annotations;
+
+import enums.CategoryType;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(java.lang.annotation.RetentionPolicy.RUNTIME)
+@Target(java.lang.annotation.ElementType.METHOD)
+public @interface FrameworkAnnotation {
+
+    String[] author();
+
+    CategoryType[] category();
+}
