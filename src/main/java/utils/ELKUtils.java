512b8ee5e7ff127636704252743b19bd785b232a
maxsudik <maxsudik@gmail.com>
2022-01-08 01:29:17 -0800

    Adding method to send testName, status and execution time to elasticsearch for displaying in kibana

---
+++ src/main/java/utils/ELKUtils.java
@@ -0,0 +1,34 @@
+package utils;
+
+import enums.ConfigProperties;
+import io.restassured.response.Response;
+import org.testng.Assert;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+public final class ELKUtils {
+    private ELKUtils() {
+    }
+
+    public static void sendDetailsToElk(String testName, String status) {
+        if (PropertyUtils.get(ConfigProperties.SENDRESULTSTOELK).equalsIgnoreCase("yes")) {
+            Map<String, String> map = new HashMap<>();
+            map.put("testName", testName);
+            map.put("status", status);
+            map.put("execution time", LocalDateTime.now().toString());
+
+            Response response = given().header("Content-Type", "application/json")
+                    .log()
+                    .all()
+                    .body(map)
+                    .post("http://localhost:9200/regression/results");
+
+            Assert.assertEquals(response.statusCode(), 201);
+            response.prettyPrint();
+        }
+    }
+}
